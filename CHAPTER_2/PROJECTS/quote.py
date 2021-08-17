bb68aae22adfd9fdbe26c87601cb5fc946019f4f
Baked Binary <86379113+bakedbins@users.noreply.github.com>
2021-08-16 23:07:50 -0600

    Delete CHAPTER_2 directory

--- CHAPTER_2/PROJECTS/quote.py
+++
@@ -1,5 +0,0 @@
-#Author: BakedBinary
-# Takes the quote & author & prints the value in a nice syntax
-author_name = "albert einstein"
-quote = '"A person who never made a mistake \n\t\t\t\t never tried anything new."'
-print(f"{author_name.title()} once said,\t {quote}")
--- CHAPTER_2/varinstrings.py
+++
@@ -1,5 +0,0 @@
-first_name = "Joseph"
-last_name = "Stalin"
-full_name = f"{first_name} {last_name}"
-message = f"hello, {full_name.title()}"
-print(message)
