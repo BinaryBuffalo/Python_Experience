cd652b607c19a90a443cfb6f1047882d732e0c88
Baked Binary <86379113+bakedbins@users.noreply.github.com>
2021-08-16 23:08:17 -0600

    Delete CHAPTER_4 directory

--- CHAPTER_4/tuples.py
+++
@@ -1,8 +0,0 @@
-# Loops 
-demon = (100, 200, 300, 400, 500)
-for Items1 in demon:
-    print(f"\t\tItem -> {Items1}") 
-#   Loops 
-lucye = (500, 400, 300, 200, 100)
-for Items2 in lucye:
-    print(f"\t\t\t\t\tItems 2 -> {Items2}")
