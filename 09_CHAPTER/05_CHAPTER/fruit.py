5554191f575345f398ff159a9144f23ab47d875a
Baked Binary <86379113+bakedbins@users.noreply.github.com>
2021-08-20 14:49:53 -0600

    Delete 09_CHAPTER/05_CHAPTER directory

--- 09_CHAPTER/05_CHAPTER/fruit.py
+++
@@ -1,8 +0,0 @@
-#   Author: BakedBinary
-fruit_list = ["Bannanas", "Apples", "Grapes"]
-if 'Bannanas' in fruit_list:
-    print(f"You really like {fruit_list[0]}")
-if 'Apples' in fruit_list:
-    print(f"You really like {fruit_list[1]}")
-if 'Grapes' in fruit_list:
-    print(f"You really like {fruit_list[2]}")
